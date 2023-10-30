81fa9d86a2d9f01bb005d2d5fa0410902fb9877a
Abhik <abhikgh.work@gmail.com>
2023-10-30 13:01:14 +0530

    updates

--- src/main/java/com/example/SpringCloudStream3Consumer/config/KafkaConfig.java
+++ src/main/java/com/example/SpringCloudStream3Consumer/config/KafkaConfig.java
@@ -3,16 +3,21 @@ package com.example.SpringCloudStream3Consumer.config;
 import com.example.SpringCloudStream3Consumer.model.Book;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.annotation.Payload;
+import reactor.core.publisher.Flux;
 
-
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Configuration
 public class KafkaConfig {
 
     @Bean
-    public Consumer<Book> consumer(){
-       return book -> System.out.println("Consumed book.." + book.getId() + "-" + book.getBookName());
+    public Function<Flux<Book>, Flux<Book>> processor() {
+        return bookFlux -> bookFlux.map(this::updateBook);
+    }
+
+    private Book updateBook(Book book) {
+        book.setBookName(book.getBookName().toUpperCase());
+        System.out.println("Book updated...");
+        return book;
     }
 }
